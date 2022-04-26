4d9ec2375eb0a57f2f71b9e15c6b75f1e5d1fc70
mdkelley02 <mdkelley02@gmail.com>
2022-04-25 19:48:23 -0600

    help me loggiwan, you're my only hope

--- src/api/views/templates/Home.php
+++ src/api/views/templates/Home.php
@@ -14,9 +14,8 @@ namespace App\api\views;
     <?php require_once __DIR__ . "/../components/SiteHeader.php"?>
     <body class="home-page">
         <div class="row g-0">
-            <?php echo __DIR__ ?>
             <div class="col-xl-2 col-lg-3">
-                <?php include "/app/src/api/views/components/SideMenu.php"?>
+                <?php include __DIR__ . "/../components/SideMenu.php"?>
             </div>
             <div class="col-xl-10 col-lg-9">
                 <?php require_once __DIR__ . "/../components/Tasks.php"?>
