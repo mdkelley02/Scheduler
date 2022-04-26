fcd4b337a86b476cbd65cd98707922d6f2c4f4be
mdkelley02 <mdkelley02@gmail.com>
2022-04-25 20:01:38 -0600

    help me loggiwan, you're my only hope

--- src/api/views/templates/Home.php
+++ src/api/views/templates/Home.php
@@ -1,6 +1,13 @@
 <?php
 namespace App\api\views;
 
+function isCurrent($route)
+{
+    $currentRoute = $_SERVER['REQUEST_URI'];
+    if ($currentRoute === $route) {
+        echo "active";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +22,30 @@ namespace App\api\views;
     <body class="home-page">
         <div class="row g-0">
             <div class="col-xl-2 col-lg-3">
-                <?php include __DIR__ . "/../components/SideMenu.php"?>
+            <style>
+                <?php echo file_get_contents(__DIR__ . "/../styles/sidemenu.css", true); ?>
+            </style>
+
+            <div class="sidemenu">
+                <div class="p-xl-4 p-lg-2">
+                    <div class="d-flex flex-column justify-space-between">
+                        <div>
+                            <a href="/public/index.php/app/">
+                                <div class="menu-item <?php isCurrent("/public/index.php/app/")?>">
+                                    <i class="fa-solid fa-house mr-3"></i>Home
+                                </div>
+                            </a>
+                        </div>
+                        <div>
+                            <div class="menu-item"><i class="fas fa-user mr-3"></i><span id="userFullName">...loading</span></div>
+                            <div id="logoutButton" class="menu-item"><i class="fas fa-sign-out-alt mr-3"></i>Logout</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <script>
+                <?php echo file_get_contents(__DIR__ . "/../scripts/sidemenu.js", true); ?>
+            </script>
             </div>
             <div class="col-xl-10 col-lg-9">
                 <?php require_once __DIR__ . "/../components/Tasks.php"?>
